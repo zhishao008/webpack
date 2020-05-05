e07417aeb0118f027dd0397bee9d83b2d17c108f
lizhi <365776419@qq.com>
2020-05-05 20:41:11 +0800

    echartsdemo

--- src/component/echartTest/index.js
+++ src/component/echartTest/index.js
@@ -1,9 +1,28 @@
 import React, { Component } from 'react';
 import echarts from 'echarts';
 export default class EchartTest extends Component {
+  constructor(props){
+    super(props);
+    this.bingtu = React.createRef();
+    this.bingtu = React.createRef();
+    this.bingtu = React.createRef();
+    this.bingtu = React.createRef();
+  }
+  componentDidMount(){
+    echarts.init(this.bingtu).setOption({
+      series: {
+          type: 'pie',
+          data: [
+              {name: 'A', value: 1212},
+              {name: 'B', value: 2323},
+              {name: 'C', value: 1919}
+          ]
+      }
+    });
+  }
   render() {
     return <div>
-        dasdasd
+        <div ref={elem => this.bingtu = elem} style={{width: "600px",height:"400px"}}></div>
     </div>
   }
 }
