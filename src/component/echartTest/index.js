d5ed08ca38f83619b8213410069426e9504544b6
李智 <lizhi@supcon.com>
2020-05-07 19:02:44 +0800

    图标类插件测试

--- src/component/echartTest/index.js
+++ src/component/echartTest/index.js
@@ -1,28 +1,38 @@
 import React, { Component } from 'react';
 import echarts from 'echarts';
+import options from "./options";
 export default class EchartTest extends Component {
   constructor(props){
     super(props);
-    this.bingtu = React.createRef();
-    this.bingtu = React.createRef();
-    this.bingtu = React.createRef();
-    this.bingtu = React.createRef();
+    this.ChartLine1 = React.createRef();
+    this.ChartLine2 = React.createRef();
+    this.ChartLine3 = React.createRef();
+    this.ChartLine4 = React.createRef();
+    this.Histogram1 = React.createRef();
+    this.Histogram2 = React.createRef();
+    this.ChartPie1 = React.createRef();
+    this.ChartPie2 = React.createRef();
   }
   componentDidMount(){
-    echarts.init(this.bingtu).setOption({
-      series: {
-          type: 'pie',
-          data: [
-              {name: 'A', value: 1212},
-              {name: 'B', value: 2323},
-              {name: 'C', value: 1919}
-          ]
-      }
-    });
+    echarts.init(this.ChartLine1).setOption(options.ChartLine1);
+    echarts.init(this.ChartLine2).setOption(options.ChartLine2);
+    echarts.init(this.ChartLine3).setOption(options.ChartLine3);
+    echarts.init(this.ChartLine4).setOption(options.ChartLine4);
+    echarts.init(this.Histogram1).setOption(options.Histogram1);
+    echarts.init(this.Histogram2).setOption(options.Histogram2);
+    echarts.init(this.ChartPie1).setOption(options.ChartPie1);
+    echarts.init(this.ChartPie2).setOption(options.ChartPie2);
   }
   render() {
-    return <div>
-        <div ref={elem => this.bingtu = elem} style={{width: "600px",height:"400px"}}></div>
+    return <div style={{ display: "flex", margin: "20px 0", flexWrap: "wrap"}}>
+        <div ref={elem => this.ChartLine1 = elem} style={{width: "560px",height:"300px", border: "1px solid #ccc"}}></div>
+        <div ref={elem => this.ChartLine2 = elem} style={{width: "560px",height:"300px", border: "1px solid #ccc"}}></div>
+        <div ref={elem => this.ChartLine3 = elem} style={{width: "560px",height:"300px", border: "1px solid #ccc"}}></div>
+        <div ref={elem => this.ChartLine4 = elem} style={{width: "560px",height:"300px", border: "1px solid #ccc"}}></div>
+        <div ref={elem => this.Histogram1 = elem} style={{width: "560px",height:"300px", border: "1px solid #ccc"}}></div>
+        <div ref={elem => this.Histogram2 = elem} style={{width: "560px",height:"300px", border: "1px solid #ccc"}}></div>
+        <div ref={elem => this.ChartPie1 = elem} style={{width: "560px",height:"300px", border: "1px solid #ccc"}}></div>
+        <div ref={elem => this.ChartPie2 = elem} style={{width: "560px",height:"300px", border: "1px solid #ccc"}}></div>
     </div>
   }
 }
---
+++ src/component/echartTest/options.js
@@ -0,0 +1,298 @@
+export default {
+  ChartLine1: {
+    xAxis: {
+        type: 'category',
+        data: ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun']
+    },
+    yAxis: {
+        type: 'value'
+    },
+    series: [{
+        data: [820, 932, 901, 934, 1290, 1330, 1320],
+        type: 'line'
+    }]
+  },
+  ChartLine2: {
+    xAxis: {
+        type: 'category',
+        boundaryGap: false,
+        data: ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun']
+    },
+    yAxis: {
+        type: 'value'
+    },
+    series: [{
+        data: [820, 932, 901, 934, 1290, 1330, 1320],
+        type: 'line',
+        areaStyle: {}
+    }]
+  },
+  ChartLine3: {
+    title: {
+        text: '折线图堆叠'
+    },
+    tooltip: {
+        trigger: 'axis'
+    },
+    legend: {
+        data: ['邮件营销', '联盟广告', '视频广告', '直接访问', '搜索引擎']
+    },
+    grid: {
+        left: '3%',
+        right: '4%',
+        bottom: '3%',
+        containLabel: true
+    },
+    toolbox: {
+        feature: {
+            saveAsImage: {}
+        }
+    },
+    xAxis: {
+        type: 'category',
+        boundaryGap: false,
+        data: ['周一', '周二', '周三', '周四', '周五', '周六', '周日']
+    },
+    yAxis: {
+        type: 'value'
+    },
+    series: [
+        {
+            name: '邮件营销',
+            type: 'line',
+            stack: '总量',
+            data: [120, 132, 101, 134, 90, 230, 210]
+        },
+        {
+            name: '联盟广告',
+            type: 'line',
+            stack: '总量',
+            data: [220, 182, 191, 234, 290, 330, 310]
+        },
+        {
+            name: '视频广告',
+            type: 'line',
+            stack: '总量',
+            data: [150, 232, 201, 154, 190, 330, 410]
+        },
+        {
+            name: '直接访问',
+            type: 'line',
+            stack: '总量',
+            data: [320, 332, 301, 334, 390, 330, 320]
+        },
+        {
+            name: '搜索引擎',
+            type: 'line',
+            stack: '总量',
+            data: [820, 932, 901, 934, 1290, 1330, 1320]
+        }
+    ]
+  },
+  ChartLine4: {
+    title: {
+        text: '堆叠区域图'
+    },
+    tooltip: {
+        trigger: 'axis',
+        axisPointer: {
+            type: 'cross',
+            label: {
+                backgroundColor: '#6a7985'
+            }
+        }
+    },
+    legend: {
+        data: ['邮件营销', '联盟广告', '视频广告', '直接访问', '搜索引擎']
+    },
+    toolbox: {
+        feature: {
+            saveAsImage: {}
+        }
+    },
+    grid: {
+        left: '3%',
+        right: '4%',
+        bottom: '3%',
+        containLabel: true
+    },
+    xAxis: [
+        {
+            type: 'category',
+            boundaryGap: false,
+            data: ['周一', '周二', '周三', '周四', '周五', '周六', '周日']
+        }
+    ],
+    yAxis: [
+        {
+            type: 'value'
+        }
+    ],
+    series: [
+        {
+            name: '邮件营销',
+            type: 'line',
+            stack: '总量',
+            areaStyle: {},
+            data: [120, 132, 101, 134, 90, 230, 210]
+        },
+        {
+            name: '联盟广告',
+            type: 'line',
+            stack: '总量',
+            areaStyle: {},
+            data: [220, 182, 191, 234, 290, 330, 310]
+        },
+        {
+            name: '视频广告',
+            type: 'line',
+            stack: '总量',
+            areaStyle: {},
+            data: [150, 232, 201, 154, 190, 330, 410]
+        },
+        {
+            name: '直接访问',
+            type: 'line',
+            stack: '总量',
+            areaStyle: {},
+            data: [320, 332, 301, 334, 390, 330, 320]
+        },
+        {
+            name: '搜索引擎',
+            type: 'line',
+            stack: '总量',
+            label: {
+                normal: {
+                    show: true,
+                    position: 'top'
+                }
+            },
+            areaStyle: {},
+            data: [820, 932, 901, 934, 1290, 1330, 1320]
+        }
+    ]
+  },
+  Histogram1: {
+    xAxis: {
+        type: 'category',
+        data: ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun']
+    },
+    yAxis: {
+        type: 'value'
+    },
+    series: [{
+        data: [120, 200, 150, 80, 70, 110, 130],
+        type: 'bar',
+        showBackground: true,
+        backgroundStyle: {
+            color: 'rgba(220, 220, 220, 0.8)'
+        }
+    }]
+  },
+  Histogram2: {
+    legend: {},
+    tooltip: {},
+    dataset: {
+        dimensions: ['product', '2015', '2016', '2017'],
+        source: [
+            {product: 'Matcha Latte', '2015': 43.3, '2016': 85.8, '2017': 93.7},
+            {product: 'Milk Tea', '2015': 83.1, '2016': 73.4, '2017': 55.1},
+            {product: 'Cheese Cocoa', '2015': 86.4, '2016': 65.2, '2017': 82.5},
+            {product: 'Walnut Brownie', '2015': 72.4, '2016': 53.9, '2017': 39.1}
+        ]
+    },
+    xAxis: {type: 'category'},
+    yAxis: {},
+    // Declare several bar series, each will be mapped
+    // to a column of dataset.source by default.
+    series: [
+        {type: 'bar'},
+        {type: 'bar'},
+        {type: 'bar'}
+    ]
+  },
+  ChartPie1: {
+    tooltip: {
+        trigger: 'item',
+        formatter: '{a} <br/>{b}: {c} ({d}%)'
+    },
+    legend: {
+        orient: 'vertical',
+        left: 10,
+        data: ['直接访问', '邮件营销', '联盟广告', '视频广告', '搜索引擎']
+    },
+    series: [
+        {
+            name: '访问来源',
+            type: 'pie',
+            radius: ['50%', '70%'],
+            avoidLabelOverlap: false,
+            label: {
+                show: false,
+                position: 'center'
+            },
+            emphasis: {
+                label: {
+                    show: true,
+                    fontSize: '30',
+                    fontWeight: 'bold'
+                }
+            },
+            labelLine: {
+                show: false
+            },
+            data: [
+                {value: 335, name: '直接访问'},
+                {value: 310, name: '邮件营销'},
+                {value: 234, name: '联盟广告'},
+                {value: 135, name: '视频广告'},
+                {value: 1548, name: '搜索引擎'}
+            ]
+        }
+    ]
+  },
+  ChartPie2: {
+    title: {
+        text: '同名数量统计',
+        subtext: '纯属虚构',
+        left: 'center'
+    },
+    tooltip: {
+        trigger: 'item',
+        formatter: '{a} <br/>{b} : {c} ({d}%)'
+    },
+    legend: {
+        type: 'scroll',
+        orient: 'vertical',
+        right: 10,
+        top: 20,
+        bottom: 20,
+        data: ["卞陈钱平路·姚祁","郝邵邬·尤","孙窦尤严·戚","赵柏严","闵柳滕"],
+      selected: {
+        "卞陈钱平路·姚祁": true,"郝邵邬·尤": false,"孙窦尤严·戚": true,"赵柏严": true,"闵柳滕": false
+      }
+    },
+    series: [
+        {
+            name: '姓名',
+            type: 'pie',
+            radius: '55%',
+            center: ['40%', '50%'],
+            data: [
+              {name: "卞陈钱平路·姚祁", value: 144063},
+              {name: "郝邵邬·尤", value: 18106},
+              {name: "孙窦尤严·戚", value: 124006},
+              {name: "赵柏严", value: 167406},
+              {name: "闵柳滕", value: 110406},
+            ],
+            emphasis: {
+                itemStyle: {
+                    shadowBlur: 10,
+                    shadowOffsetX: 0,
+                    shadowColor: 'rgba(0, 0, 0, 0.5)'
+                }
+            }
+        }
+    ]
+  }
+}
